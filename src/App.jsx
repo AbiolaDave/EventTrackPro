b7e79a22ccf01bde125e44760b734436424d4c3f
Abiola <abioladave24@gmail.com>
2024-08-29 04:39:22 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/eventtrackpro/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
